44834e7a58f980a45376d28bf9aba1d9410b8389
rsaxena23 <raunaqsaxena23@gmail.com>
2015-10-10 20:03:31 -0400

    ListView implemented.

--- app/src/main/java/fbaapplication/fba/com/fbapplication/HomeScreenTry.java
+++ app/src/main/java/fbaapplication/fba/com/fbapplication/HomeScreenTry.java
@@ -10,39 +10,109 @@ import android.widget.AdapterView;
 import android.view.*;
 import android.widget.Toast;
 import java.util.ArrayList;
-
+import android.content.Intent;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.app.Activity;
 
 public class HomeScreenTry extends AppCompatActivity {
 
     ListView lv1;
-  //  String arr[]={"Group 1"," Group 2"," Group 3"," Group 4"," Group 5"," Group 6"," Group 7"," Group 8"};
-    ArrayList <RowData>data;
+    String arr[]={"Group 1"," Group 2"," Group 3"," Group 4"," Group 5"," Group 6"," Group 7"," Group 8"};
+    ArrayList <RowData>not_list=new ArrayList<RowData>();
+    RowData notiL[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen_try);
 
+        not_list.add(0, new RowData("Group 1", R.mipmap.ic_launcher));
+        not_list.add(1, new RowData("Group 2", R.mipmap.ic_launcher));
+        not_list.add(2, new RowData("Group 3", R.mipmap.ic_launcher));
+        not_list.add(3, new RowData("Group 4", R.mipmap.ic_launcher));
+        not_list.add(4, new RowData("Group 5", R.mipmap.ic_launcher));
+        not_list.add(5, new RowData("Group 6", R.mipmap.ic_launcher));
+        not_list.add(6, new RowData("Group 7", R.mipmap.ic_launcher));
+        not_list.add(7, new RowData("Group 8", R.mipmap.ic_launcher));
+        not_list.add(8, new RowData("Group 9", R.mipmap.ic_launcher));
+
+
+
+
         lv1=(ListView)findViewById(R.id.listTry);
-        ListAdapter la= new ArrayAdapter<String>(this, R.layout.tval,arr);
+        ListAdapter la= new ArrayAdapterMod(this, R.layout.tval,not_list);
         lv1.setAdapter(la);
 
-        lv1.setOnItemClickListener(new OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position,
-                                    long arg3)
-            {
-                String value = (String)adapter.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
-
-            }
-        });
+        lv1.setOnItemClickListener(new OnItemClickListenerMod());
 
     }
 }
 
+class ArrayAdapterMod extends ArrayAdapter<RowData>
+{
+
+    Context mContext;
+
+    int layoutResourceId;
+
+    ArrayList <RowData> data ;
+
+    public ArrayAdapterMod(Context mContext, int layoutResourceId, ArrayList<RowData> data) {
+
+
+
+        super(mContext, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.mContext = mContext;
+
+        this.data = data;
+
+    }
+
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView==null){
+
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            convertView = inflater.inflate(layoutResourceId, parent, false);
+        }
+        RowData value = data.get(position);
+        TextView textViewItem = (TextView) convertView.findViewById(R.id.listTextValue);
+        textViewItem.setText(value.data);
+        ImageView iv=(ImageView)convertView.findViewById(R.id.listImage);
+        iv.setBackgroundResource(value.image_id);
+        return convertView;
+    }
+
+}
+
+class OnItemClickListenerMod implements OnItemClickListener
+{
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Context cc=view.getContext();
+        TextView tv1= (TextView) view.findViewById(R.id.listTextValue);
+        String output= tv1.getText().toString();
+        Toast.makeText(cc,"Out: "+output,Toast.LENGTH_SHORT).show();
+
+    }
+}
+
+
 class RowData
 {
     String data;
-    int image_id=R.mipmap.ic_launcher;
+    int image_id;
+    public RowData(String temp,int img)
+    {
+        data= temp;
+        image_id=img;
+    }
+
 }
--- app/src/main/java/fbaapplication/fba/com/fbapplication/MainActivity.java
+++ app/src/main/java/fbaapplication/fba/com/fbapplication/MainActivity.java
@@ -8,18 +8,51 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.CallbackManager;
+import com.facebook.login.LoginResult;
+import com.facebook.FacebookCallback;
+import android.content.Intent;
+import com.facebook.FacebookException;
 
 public class MainActivity extends AppCompatActivity {
 
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(getApplicationContext(),"True",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(getApplicationContext(),"Can",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(getApplicationContext(),"ERR:"+exception.getMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +62,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
